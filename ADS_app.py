b36492baf69bd7d8e0d8fe924226ff75d6cc3eed
DasaV7 <dasan.varathan@gmail.com>
2026-05-29 23:20:22 -0700

    Update ADS_app.py

--- ADS_app.py
+++ ADS_app.py
@@ -763,7 +763,7 @@ def render_home():
     
     <div class="cinematic-banner">
         <video class="cinematic-video" autoplay muted loop playsinline>
-            <source src="YOUR_VIDEO_FILE.mov" type="video/quicktime">
+            <source src="ADS_VIDEO_2.mov" type="video/quicktime">
         </video>
     </div>
     """, unsafe_allow_html=True)
